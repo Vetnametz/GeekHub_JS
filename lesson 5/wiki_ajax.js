a447904710dd7223f410030a3cabe0230fad8e4a
Vetnametz <vitaltrachenko@gmail.com>
2014-11-14 01:14:42 +0200

    initial commit

---
+++ lesson 5/wiki_ajax.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#search-btn').on('click', function () {
+        var inputVal = $('#search-str').val();
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + inputVal,
+            dataType: "jsonp"
+        })
+            .done(function( data ) {
+                console.log(data[1]);
+                var searchKeyWord = 'Your searching word is: ' + data[1][0];
+                $('#search-res').append(searchKeyWord);
+                $(data[1]).slice(1).each(function(key1, val1) {
+                    var serchResults = '<li>' + val1 + '</li>';
+                    $('#search-key').append(serchResults);
+                });
+            });
+    });
+});
+
