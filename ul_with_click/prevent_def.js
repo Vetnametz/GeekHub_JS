90695ea2571a9d464f045152e7e03b2e4e7d6914
Vetnametz <vitaltrachenko@gmail.com>
2014-12-04 23:31:50 +0200

    add friends

--- ul_with_click/prevent_def.js
+++ ul_with_click/prevent_def.js
@@ -1,16 +1,25 @@
 jQuery(function ($) {
-    $('.wrapper ul').click(function (event) {
-        var target = event.target;
-        if ($(target).prop("tagName") === 'INPUT' && $(target).hasClass('marked')) {
-            $(target).removeClass('marked');
-        } else if ($(target).hasClass('remove-this')){
-            $(target).parent().remove();
-        } else if ($(target).prop("tagName") === 'INPUT' && !($(target).hasClass('marked'))) {
-            $(target).addClass('marked');
+
+    $('ul#friends').click(function (event) {
+        var target = event.target,
+            $target = $(target),
+            marked = !($target.hasClass('marked'));
+
+        $target.toggleClass('marked', marked);
+
+        if ($target.hasClass('remove-this')) {
+            $target.closest('li').remove();
         }
         event.stopPropagation();
     });
-    $('.wrapper ul li a#remove-all').click(function (event) {
-        $('.wrapper ul li input.marked').parent().remove();
+
+    $('.wrapper ul#control li a#remove-all').click(function (event) {
+        $('.wrapper ul#friends li input.marked').parent().remove();
     });
+
+    $('.wrapper ul#control li a#add-new').click(function (event) {
+        var addNew = '<li><label></label><input value="" type="text"/><a class="remove-this" href="#">Удалить</a></li>';
+        $('#friends').append(addNew);
+    });
+
 });
